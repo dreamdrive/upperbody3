e20d3fbd8914b1ab0cfeba1c9676c8daf26f8192
Hirokazu Onomichi <hirokazu@dream-drive.net>
2020-12-20 10:45:11 +0900

    Update js2dynamixel.cpp

--- upperbody3_control/src/js2dynamixel.cpp
+++ upperbody3_control/src/js2dynamixel.cpp
@@ -128,71 +128,22 @@ int main(int argc, char **argv)
     printf("Failed to sync write position\n");
   }
 
-
-
-
-
-
   ros::init(argc, argv, "js2dynamixel");   // ノードの初期化
   ros::NodeHandle nh;                             // ノードハンドラ
 
   ros::Subscriber sub_joints;                     // サブスクライバの作成
   sub_joints = nh.subscribe("/skeleton/joint_states", 1, monitorJointState_callback);
 
-  //ros::Publisher arm_pub;                         //パブリッシャの作成
-  //arm_pub = nh.advertise<trajectory_msgs::JointTrajectory>("/dynamixel_workbench/joint_trajectory",1);
-
   ros::Rate loop_rate(100);  // 制御周期45Hz(サーボの更新が20msecなのですが、50Hzだと余裕ない感じ)
 
-  //trajectory_msgs::JointTrajectory jtp0;
-  
-  // jtp0.header.frame_id = "base_link";   // ポーズ名（モーション名)
-
-  // jtp0.joint_names.resize(5); // 名前
-  // jtp0.joint_names[0] ="waist";
-  // jtp0.joint_names[1] ="right_j1";
-  // jtp0.joint_names[2] ="right_j2";
-  // jtp0.joint_names[3] ="left_j1";
-  // jtp0.joint_names[4] ="left_j2";
-  
   joint_name[0].data = "waist";
   joint_name[1].data = "right_j1";
   joint_name[2].data = "right_j2";
   joint_name[3].data = "left_j1";
   joint_name[4].data = "left_j2";
 
-  // jtp0.points.resize(1);                // ポーズは一つだけ
-
-  // jtp0.points[0].positions.resize(5); // ポジションを1個設定  
-  // jtp0.points[0].velocities.resize(5); // ポジションを5個設定  
-  // jtp0.points[0].accelerations.resize(5); // ポジションを5個設定  
-  // jtp0.points[0].effort.resize(5); // ポジションを5個設定  
-
-  // for(i=0;i<5;i++){
-  //   jtp0.points[0].positions[i] = 0.0;
-  // }
-
-  // jtp0.points[0].time_from_start = ros::Duration(0.02);  //実行時間20msec (10msecだと動作しない;;)
 
   while (ros::ok()) { // 無限ループ
-
-  //   jtp0.header.stamp = ros::Time::now();
-    
-  //   jtp0.points[0].positions[0] = 0.0;  // 腰から腰
-  //   jtp0.points[0].positions[1] = joint_pos[1].data;  // 右から右
-  //   jtp0.points[0].positions[2] = joint_pos[2].data;  // 右から右
-  //   jtp0.points[0].positions[3] = -joint_pos[1].data;  // 右から左(マスター＆スレーブ)
-  //   jtp0.points[0].positions[4] = joint_pos[2].data;  // 右から左(マスター＆スレーブ)
-
-  //  // jtp0.points[0].velocities[3] = 0.05;  // 右から左(マスター＆スレーブ)
-  //  // jtp0.points[0].velocities[4] = 0.02;  // 右から左(マスター＆スレーブ)
-
-  //   arm_pub.publish(jtp0);
-
-    // ROS_INFO("torikometeru? = %f",joint_pos[1].data);
-
-
-
 
 
   for(i=0;i<5;i++){
